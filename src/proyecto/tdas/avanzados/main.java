b1f31b2f02312394682cc9684cf7d24da915c42a
jairoaguilarts <jairoaguilar1219@gmail.com>
2022-09-26 19:36:05 -0600

    TDAGrafo cargado de archivo

--- src/proyecto/tdas/avanzados/main.java
+++ src/proyecto/tdas/avanzados/main.java
@@ -90,20 +90,33 @@ public class main {
                     break;
                 }
                 case 2 -> {
+                    TDAGrafo grafo = new TDAGrafo();
                     int opcionGrafos = menuGrafos();
                     while(opcionGrafos != 4) {
                         switch(opcionGrafos) {
                             case 1 -> { //Leer grafo de archivo
                                 System.out.println("Ingrese el nombre del archivo con el grafo: ");
-                                String archivo = sc.next(), texto ="";
+                                String archivo = sc.next();
                                 try {
                                     //Lee archivo
                                     BufferedReader bf = new BufferedReader(new FileReader("./Carpeta Grafos/" + archivo + ".txt"));
-                                    String bfRead; 
-                                    while((bfRead = bf.readLine()) != null) {
-                                        texto += bfRead;
+                                    String linea; 
+                                    int iteracion = 0, size = 0, fila = 0;
+                                    Vertice matriz[][] = null;
+                                    while((linea = bf.readLine()) != null) {
+                                        if(iteracion++ == 0) {
+                                            size = Integer.parseInt(linea);
+                                            matriz = new Vertice[size][size];
+                                        } else {
+                                            String arreglo[] = linea.split(" ");
+                                            Vertice arregloVertices[] = new Vertice[size];
+                                            for(int i = 0;i < arreglo.length; i++) {
+                                                arregloVertices[i] = new Vertice(Integer.parseInt(arreglo[i]));
+                                            }
+                                            matriz[fila++] = arregloVertices;
+                                        }
                                     }
-                                    
+                                    grafo.setMatrizAdyacencia(matriz);
                                 } catch (IOException e) {
                                     System.out.println("Error: " + e.getMessage());
                                     e.printStackTrace();
@@ -111,7 +124,7 @@ public class main {
                                 break;
                             }
                             case 2 -> { //Prim
-                                GrafoPrim prim = new GrafoPrim(5);
+                                GrafoPrim prim = (GrafoPrim)grafo;
                                 //prim.automatic_Fill();
                                 prim.algoritmoPrim();
                                 break;
